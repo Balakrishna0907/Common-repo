e32eb8b9d8720e129d676cfb40683f4805afad42
Balakrishna <balakrishna090708@gmail.com>
2025-09-11 20:08:37 +0530

    Update presentation.cpp

--- presentation/presentation.cpp
+++ presentation/presentation.cpp
@@ -14,10 +14,10 @@ int main()
 {
     char op;
     int i=0;
-    struct voter;
     do
     {
         cout<<"Enter your Name: "<<endl;
+        cin.ignore(10000,'\n'); //to clear input buffer
         cin.getline(v[i].name,50);
         cout<<"Enter your VoterID: "<<endl;
         cin.getline(v[i].voterid,20);
@@ -25,7 +25,6 @@ int main()
         cin>>v[i].age;
         cout<<"Enter your Gender (M/F/O): "<<endl;
         cin>>v[i].gender;
-        cin.ignore(10000,'\n'); //to clear input buffer
         if(v[i].age>=18) //age verification
         {
             ofstream file("VoterDetails-presentation",ios::app); //initialising a file & appending datas to it
